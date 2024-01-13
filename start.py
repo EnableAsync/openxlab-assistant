a05a3d71eb53c827a18c8abb56ff6360b42f4def
unknown <raisinata@foxmail.com>
2024-01-13 22:56:29 +0800

    update

--- start.py
+++ start.py
@@ -3,11 +3,9 @@ from openxlab.model import download
 
 def Download():
     download(model_repo='OpenLMLab/InternLM-chat-7b',output='/home/xlab-app-center/InternLM-chat-7b')
+    download(model_repo='EnableAsync/openxlab-assistant',output="./hf")
 
 Download()
 
 os.system('xtuner convert merge /home/xlab-app-center/InternLM-chat-7b ./hf ./hf-merge --max-shard-size 2GB')
-
-
-
 os.system('streamlit run ./InternLM/app.py --server.address=0.0.0.0 --server.port 7860')
